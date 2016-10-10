9219c34142c7bd3eddda2284811a0ff78ae8ac88
jmoore <josh@glencoesoftware.com>
2016-10-10 11:50:25 +0200

    In-place improvements
     * Add Getting_Started notebook
     * Remove show_ functions in favor of `display()`
     * Add simple_colocalisation from Will. See:
    
    https://gist.github.com/will-moore/585f454439ceee94dffdfda284ef090f#file-simple_colocalisation-py

--- library/idr_demo.py
+++ library/idr_demo.py
@@ -34,22 +34,29 @@ def images_by_phenotype(conn,
     return list(conn.getObjectsByAnnotations("Image", ann_ids))
 
 
-def show_thumbnail(image, *args, **kwargs):
-    """
-    Given a loaded Image object, retrieve the thumbnail and display it
-    inline in the notebook. Arguments are passed directly to the
-    getThumbnail method.
-    """
-    return IPython.display.display(IPython.display.Image(
-        data=image.getThumbnail(*args, **kwargs)))
 
+def simple_colocalisation(image):
+    """
+    Perform a simple comparison of the red and green channels.
 
-def show_image(image, *args, **kwargs):
+    Pearson coefficient described at https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3074624/
     """
-    Like show_thumbnail, but display a full-size image.
-    """
-    return IPython.display.display(IPython.display.Image(
-        data=image.renderJpeg(*args, **kwargs)))
+    p = image.getPrimaryPixels()
+
+    # get 2D planes and reshape to 1D array
+    r = p.getPlane(0,0,0)
+    red = r.reshape(r.size)
+    g = p.getPlane(0,1,0)
+    green = g.reshape(g.size)
+
+    # pearson colocalistion coefficient
+    from scipy.stats import pearsonr
+    pearsonr(red, green)
+
+    # scatter plot
+    import matplotlib.pyplot as plt
+    plt.scatter(red, green)
+    plt.show()
 
 
 def connection():
