4b67c522b0fcf93427e02caa60103b11c460dcca
bramalingam <b.ramalingam@dundee.ac.uk>
2017-07-14 11:48:31 +0100

    update __init__.py to load all new methods

--- library/idr/__init__.py
+++ library/idr/__init__.py
@@ -1,5 +1,15 @@
 from connections import connection
+from connections import createHTTPsession
+
 from attributes import attributes_by_attributes
 from attributes import annotation_ids_by_field
-from images import images_by_phenotypes
+from attributes import get_phenotypes_for_gene
+
+from images import images_by_phenotype
 from images import simple_colocalisation
+
+from externalDBs import genes_of_interest_fromGO
+from externalDBs import genes_of_interest_fromString
+from externalDBs import network_of_interest
+from externalDBs import get_entrezid
+from externalDBs import get_ensembleid
